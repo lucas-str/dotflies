f6fc3d218aa2202725d4a60aa015368931bd3e75
lucas-str <lucas.sturelle@ik.me>
2025-12-04 11:16:23 +0100

    nvim: enable lsp configs

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,9 +1,11 @@
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- Python
+vim.lsp.enable("pyright")
 vim.lsp.config("pyright", { capabilities = capabilities })
 
 -- Lua
+vim.lsp.enable("lua_ls")
 vim.lsp.config("lua_ls", {
     settings = {
         Lua = {
@@ -21,10 +23,12 @@ vim.lsp.config("lua_ls", {
 })
 
 -- GDScript
+vim.lsp.enable("gdscript")
 vim.lsp.config("gdscript", { capabilities = capabilities })
 
 -- Rust
 --vim.lsp.config("rust_analyzer", { settings = { ["rust-analyzer"] = {} } })
+vim.lsp.enable("rust_analyzer")
 vim.lsp.enable("rust_analyzer")
 
 vim.api.nvim_create_user_command("LspRename", function()
