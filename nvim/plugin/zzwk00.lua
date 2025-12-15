2abd457e6107be6f1a2479f81c4cc7b012126271
lucas-str <lucas.sturelle@ik.me>
2025-12-15 14:00:45 +0100

    nvim: groovy nvim-treesitter and lspconfig

--- nvim/plugin/zzwk00.lua
+++ nvim/plugin/zzwk00.lua
@@ -7,25 +7,49 @@ vim.api.nvim_create_user_command("Date", function()
 end, { desc = "Insert date to current line" })
 
 -- nvim-treesitter
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.groovy = {
-    install_info = {
-        url = "~/Workspace/External/tree-sitter-groovy", -- local path or git repo
-        files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-        -- optional entries:
-        branch = "hotfix/disable-groovy-doc", -- default branch in case of git repo if different from master
-        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
-    },
-    -- filetype = "groovy", -- if filetype does not match the parser name
-}
+-- Old configuration (master branch):
+--local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--parser_config.groovy = {
+--    install_info = {
+--        url = "~/Workspace/External/tree-sitter-groovy", -- local path or git repo
+--        files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--        -- optional entries:
+--        branch = "hotfix/disable-groovy-doc", -- default branch in case of git repo if different from master
+--        -- generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+--        -- requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+--    },
+--    -- filetype = "groovy", -- if filetype does not match the parser name
+--}
+-- After main branch (not working):
+--vim.api.nvim_create_autocmd("User", {
+--    pattern = "TSUpdate",
+--    callback = function()
+--        require("nvim-treesitter.parsers").groovy = {
+--            install_info = {
+--                path = "https://github.com/zimbulang/tree-sitter-zimbu",
+--                files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+--                -- optional entries:
+--                queries = "queries/neovim", -- also install queries from given directory
+--            },
+--        }
+--    end,
+--})
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = { "groovy" },
+    callback = function()
+        -- syntax highlighting, provided by Neovim
+        vim.treesitter.start()
+        -- indentation, provided by nvim-treesitter
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+})
 
 -- Conform
 local conform = require("conform")
 conform.formatters_by_ft.python = { "isort", "black" }
 conform.formatters.isort = { prepend_args = { "--profile", "black" } }
 conform.formatters_by_ft.groovy = { "npm-groovy-lint" }
--- conform.formatters["npm-groovy-lint"] = { prepend_args = { "--failon", "none" } }
+-- `npm install -g npm-groovy-lint`
 conform.formatters["npm-groovy-lint"] = {
     command = "npm-groovy-lint",
     -- args = { "--failon", "none", "--fix", "-" },
@@ -38,6 +62,17 @@ local lint = require("lint")
 lint.linters_by_ft.python = { "pylint", "mypy" }
 lint.linters_by_ft.groovy = { "npm-groovy-lint" }
 lint.linters["npm-groovy-lint"].args = { "--failon", "none", "--loglevel", "warning" }
+
+-- lspconfig
+-- https://github.com/GroovyLanguageServer/groovy-language-server
+vim.lsp.enable("groovyls")
+vim.lsp.config("groovyls", {
+    cmd = {
+        "java",
+        "-jar",
+        "/home/lsturelle/Workspace/External/groovy-language-server/build/libs/groovy-language-server-all.jar",
+    },
+})
 
 -- ALE
 -- fixers
